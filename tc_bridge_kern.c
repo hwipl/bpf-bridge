f07c680acd815ec2ffc2a25b7386b62477c4a218
hwipl <33433250+hwipl@users.noreply.github.com>
2021-02-02 23:22:28 +0100

    get source mac address from packet in skb
    Signed-off-by: hwipl <33433250+hwipl@users.noreply.github.com>

--- tc_bridge_kern.c
+++ tc_bridge_kern.c
@@ -5,6 +5,9 @@
 /* tc header */
 #include <linux/pkt_cls.h>
 
+/* ethernet header */
+#include <net/ethernet.h>
+
 /* set license to gpl */
 char _license[] SEC("license") = "GPL";
 
@@ -12,9 +15,22 @@ char _license[] SEC("license") = "GPL";
 SEC("bridge_forward")
 int _bridge_forward(struct __sk_buff *skb)
 {
-	/* get ingress interface of packet */
+	void *data_end = (void *)(long)skb->data_end;
+	void *data = (void *)(long)skb->data;
+	struct ether_header *eth = data;
+
+	/* check size of packet */
+	if (data + sizeof(*eth) > data_end)
+		return TC_ACT_OK;
+
+	/* get ingress interface and source mac of packet */
 	__u32 ifindex = skb->ingress_ifindex;
+	uint8_t *from = eth->ether_shost;
 	bpf_printk("bpf_bridge: if: %d", ifindex);
+	bpf_printk("bpf_bridge: from[0:3]: %x:%x:%x:", from[0], from[1],
+		   from[2]);
+	bpf_printk("bpf_bridge: from[3:6]: %x:%x:%x", from[3], from[4],
+		   from[5]);
 
 	return TC_ACT_OK;
 }
