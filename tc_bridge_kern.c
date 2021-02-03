e9b0cf7a5d5be4f9b9cd0b9d28be367214c6659f
hwipl <33433250+hwipl@users.noreply.github.com>
2021-02-03 23:35:12 +0100

    store source mac and ingress interface in lru hash map
    Signed-off-by: hwipl <33433250+hwipl@users.noreply.github.com>

--- tc_bridge_kern.c
+++ tc_bridge_kern.c
@@ -11,6 +11,27 @@
 /* set license to gpl */
 char _license[] SEC("license") = "GPL";
 
+/* map definitions */
+#define PIN_GLOBAL_NS		2
+struct bpf_elf_map {
+	__u32 type;
+	__u32 size_key;
+	__u32 size_value;
+	__u32 max_elem;
+	__u32 flags;
+	__u32 id;
+	__u32 pinning;
+};
+
+/* hash map for mapping mac address to interface index */
+struct bpf_elf_map SEC("maps") bpf_bridge_mac_table = {
+	.type = BPF_MAP_TYPE_LRU_HASH,
+	.size_key = sizeof(struct ether_addr),
+	.size_value = sizeof(__u32),
+	.pinning = PIN_GLOBAL_NS,
+	.max_elem = 2048,
+};
+
 /* forward packets */
 SEC("bridge_forward")
 int _bridge_forward(struct __sk_buff *skb)
@@ -23,14 +44,10 @@ int _bridge_forward(struct __sk_buff *skb)
 	if (data + sizeof(*eth) > data_end)
 		return TC_ACT_OK;
 
-	/* get ingress interface and source mac of packet */
+	/* add ingress interface and source mac of packet to macs map */
 	__u32 ifindex = skb->ingress_ifindex;
-	uint8_t *from = eth->ether_shost;
-	bpf_printk("bpf_bridge: if: %d", ifindex);
-	bpf_printk("bpf_bridge: from[0:3]: %x:%x:%x:", from[0], from[1],
-		   from[2]);
-	bpf_printk("bpf_bridge: from[3:6]: %x:%x:%x", from[3], from[4],
-		   from[5]);
+	uint8_t *src_mac = eth->ether_shost;
+	bpf_map_update_elem(&bpf_bridge_mac_table, src_mac, &ifindex, BPF_ANY);
 
 	return TC_ACT_OK;
 }
