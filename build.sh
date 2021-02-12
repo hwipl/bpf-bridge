febfc224d9ebcf8696a932c62ffab0ab4ac21455
hwipl <33433250+hwipl@users.noreply.github.com>
2021-02-12 16:52:03 +0100

    build.sh: add command line argument for test mode build
    Signed-off-by: hwipl <33433250+hwipl@users.noreply.github.com>

--- build.sh
+++ build.sh
@@ -3,6 +3,10 @@
 CLANG=/usr/bin/clang
 LLC=/usr/bin/llc
 
-$CLANG tc_bridge_user.c -o tc_bridge_user -lbpf
+if [[ $1 == "test" ]]; then
+	$CLANG tc_bridge_user.c -o tc_bridge_user -lbpf -D TEST
+else
+	$CLANG tc_bridge_user.c -o tc_bridge_user -lbpf
+fi
 $CLANG -O2 -emit-llvm -c tc_bridge_kern.c -o - -fno-stack-protector | \
 	$LLC -march=bpf -filetype=obj -o tc_bridge_kern.o
